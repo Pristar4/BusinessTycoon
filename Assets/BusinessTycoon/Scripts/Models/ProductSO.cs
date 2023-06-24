b655ce4b7b41f7cbdb2ef7865f1f69435ad679d3
Felix Jung <felix.j.jung@gmail.com>
2023-06-24 08:47:16 +0200

    feat: ProductionPanel UI + TotalCost calculation

--- Assets/BusinessTycoon/Scripts/Models/ProductSO.cs
+++ Assets/BusinessTycoon/Scripts/Models/ProductSO.cs
@@ -14,32 +14,35 @@ namespace BT.Scripts.Models {
   [CreateAssetMenu(fileName = "new_product", menuName = "Production/Product",
       order = 0)]
   public class ProductSo : ScriptableObject {
-    public Vector2Int PriceRange => priceRange;
-
-    [ShowInInspector]
-    public int MiddlePrice => (priceRange.x + priceRange.y) / 2;
-
-    public string ContractsAppear
-      =>
-          // 4 Quarters per year
-          $"Y{contractsAppear / 4 + 1} Q{contractsAppear % 4 + 1}";
-
-    [ShowInInspector]
-    /*public int GrowthPerQuarter =>
-        // Pseudo code:
-        // Retrieve the middlePrices for the last four quarters:
-        // Calculate the average increase
-        // Return the result
-        growthPerQuarter;*/
-    #region Serialized Fields
     [SerializeField]
     private Vector2Int priceRange;
+    #region Fields
     private int middlePrice;
     public int contractsAppear;
     public int productDemand;
     public int contracts;
     public int growthInPricePerQuarter;
     public int growthPerQuarter;
+    #endregion
+
+    public string ContractsAppear
+      =>
+          // 4 Quarters per year
+          $"Y{contractsAppear / 4 + 1} Q{contractsAppear % 4 + 1}";
+
+    public int MaterialCostPerUnit { get; }
+
+    [ShowInInspector]
+    public int MiddlePrice => (priceRange.x + priceRange.y) / 2;
+
+    public Vector2Int PriceRange => priceRange;
+    
+    /*public int GrowthPerQuarter =>
+        // Pseudo code:
+        // Retrieve the middlePrices for the last four quarters:
+        // Calculate the average increase
+        // Return the result
+        growthPerQuarter;*/
     public void CalculateDemand(int currentTurn) {
       // Don't increase demand until contracts appear
       if (currentTurn < contractsAppear)
@@ -48,6 +51,5 @@ namespace BT.Scripts.Models {
         productDemand += growthPerQuarter;
 
     }
-    #endregion
   }
 }
--- Assets/BusinessTycoon/Scripts/Panels/ProductionPanel.cs
+++ Assets/BusinessTycoon/Scripts/Panels/ProductionPanel.cs
@@ -7,25 +7,50 @@
 #endregion
 #region
 using BT.Scripts.Interfaces;
+using BT.Scripts.Managers;
 using BT.Scripts.Models;
+using TMPro;
 using UnityEngine;
 #endregion
 
 namespace BT.Scripts.Panels {
   public class ProductionPanel : MonoBehaviour, IPanel {
-
     [SerializeField] private GameObject factoryPanel;
+    [SerializeField] private TMP_Text totalCostText;
+    private int totalCost;
+    private Company company;
     #region IPanel Members
     public void Initialize(PanelData data = null) {
-      // TODO: Implement
+      company = ManagerProvider.Current.PlayerManager.GetPlayerCompany();
     }
     public void SetActive(bool isActive) {
       gameObject.SetActive(isActive);
       factoryPanel.SetActive(isActive);
     }
     public void UpdatePanel() {
-      // TODO: Implement
+      totalCost = CalculateTotalCost();
+      totalCostText.text = "Total Cost: " + totalCost + "€";
     }
     #endregion
+    private int CalculateTotalCost() {
+      totalCost = 0;
+      foreach (var factoryData in company.FactoryInventory)
+        totalCost += factoryData.CalculateProductionCost() +
+                     CalculateMaterialCost(factoryData) +
+                     CalculateLaborCost(factoryData);
+      return totalCost;
+    }
+    private static int CalculateMaterialCost(FactoryData factoryData) {
+      return factoryData.Factory.productProduced.MaterialCostPerUnit *
+             factoryData.CurrentOutput;
+    }
+    private int CalculateLaborCost(FactoryData factoryData) {
+      var wage = 110;
+      // 8 hours per day, 22 days per month, 3 months per quarter = 528 hours per quarter
+      var hoursPerQuarter = 528;
+      return factoryData.Factory.LaborForce * hoursPerQuarter * wage;
+
+    }
+    
   }
 }
